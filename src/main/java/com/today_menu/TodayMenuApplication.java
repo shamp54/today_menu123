a7b672afef7309ec86224de79b43109226ae8346
shamp54 <121779109+shamp54@users.noreply.github.com>
2023-01-03 10:31:07 +0900

    Initial commit

---
+++ src/main/java/com/today_menu/TodayMenuApplication.java
@@ -0,0 +1,13 @@
+package com.today_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/today_menu/TodayMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.today_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
